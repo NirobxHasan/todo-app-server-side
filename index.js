36eb55f3bc20fe31ea4ac8f5f8a9721bc0d0d5ef
Nirob Hasan <thenirobhasan@gamil.com>
2021-12-03 14:56:01 +0600

    JWT Implemented

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
 const ObjectID = require('mongodb').ObjectId;
+const { initializeApp } = require('firebase-admin/app');
+var admin = require("firebase-admin");
 require('dotenv').config();
-
 const app = express();
+
+//firebase admin initailization
+var serviceAccount = require('./todo-app-nh-firebase-adminsdk-ehx7n-2dfd97df11.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 //middleware
 app.use(cors())
@@ -14,6 +22,20 @@ const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jiiff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Verify using JWT
+async function verifyToken(req,res,next){
+    if(req.headers?.authorization?.startsWith('Bearer ')){
+        const idToken = req.headers.authorization.split(" ")[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(idToken);
+            req.decodedUserEmail = decodedUser.email;
+        } catch (error) {
+            
+        }
+    }
+    next();
+}
 
 async function run() {
     try {
@@ -36,7 +58,6 @@ async function run() {
       app.put('/users/:email', async(req,res)=>{
         const user = req.body;
         const prepEmail = req.params.email;
-        console.log(prepEmail,user);
         const filter = {email: prepEmail }
         const options = { upsert: true };
         const updateDoc = {
@@ -55,6 +76,38 @@ async function run() {
           const result = await usersCollection.deleteOne({email:email})
           res.json(result)
       })
+      //check is admin or not
+      app.get('/users_admin/:email', async(req,res)=>{
+        const email = req.params.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query)
+        let isAdmin = false;
+        if(user?.role === 'admin'){
+          isAdmin = true;
+        }
+        res.json({'admin': isAdmin});
+      })
+
+      //Get All User
+      app.get('/users', async(req,res)=>{
+          const cursor = usersCollection.find({})
+          const users= await cursor.toArray();
+          res.json(users)
+      })
+      //delete single user
+      app.delete('/user/:id', async(req,res)=>{
+        const id = req.params.id;  
+        const result = await usersCollection.deleteOne({_id:ObjectID(id)})
+        res.json(result)
+      })
+
+      //Get single user
+      app.get('/user/:id', async(req,res)=>{
+        const id = req.params.id;
+        const user = await usersCollection.findOne({_id:ObjectID(id)});
+        res.json(user)
+      })
+      //
     
       //Notes post
       app.post('/notes', async(req,res)=>{
@@ -65,7 +118,6 @@ async function run() {
                 const filter =   {email:data.email};
                 const newlimit = user.subscription.limit-1;
                 const sub ={...user.subscription,limit : newlimit}
-                console.log(sub);
                 const updateDoc = {
                     $set: {
                         subscription:sub
@@ -79,12 +131,21 @@ async function run() {
       })
 
       //Get Notes
-      app.get('/notes/:email', async(req,res)=>{
+      app.get('/notes/:email', verifyToken, async(req,res)=>{
+          
           const email = req.params.email;
-          const query = {email: email};
-          const cursor = notesCollection.find(query);
-          const notes = await cursor.toArray();
-          res.json(notes)
+          if(req.decodedUserEmail === email){
+             
+            const query = {email: email};
+            const cursor = notesCollection.find(query);
+            const notes = await cursor.toArray();
+            res.json(notes)
+           
+          }
+          else{
+              res.status(401).json({message:'User not authorized'})
+          }
+          
       })
 
       //Get Single Note
@@ -104,7 +165,6 @@ async function run() {
           const options = { upsert: true };
           const newlimit = user.subscription.limit+1;
           const sub ={...user.subscription,limit : newlimit}
-           console.log(sub);
            const updateDoc = {
             $set: {
                 subscription:sub
@@ -137,14 +197,20 @@ async function run() {
 
       //Filter Note
 
-      app.get('/filterNotes', async(req,res)=>{
+      app.get('/filterNotes',verifyToken, async(req,res)=>{
         const email = req.query.email;
-        const date = new Date(req.query.date).toLocaleDateString();
-        console.log(email,date);
-        const query = {email:email, date:date}
-        const cursor = notesCollection.find(query);
-        const notes = await cursor.toArray();
-        res.json(notes);
+        if(req.decodedUserEmail === email){
+            const date = new Date(req.query.date).toLocaleDateString();
+            // console.log(email,date);
+            const query = {email:email, date:date}
+            const cursor = notesCollection.find(query);
+            const notes = await cursor.toArray();
+            res.json(notes);
+        }
+        else{
+            res.status(401).json({message:'User not authorized'})
+        }
+        
       })
 
 
@@ -156,11 +222,43 @@ async function run() {
           const subscriptions = await cursor.toArray();
           res.json(subscriptions);
       })
+
+    
       //get a package
       app.get('/subscriptions/:id', async(req,res)=>{
           const id = req.params.id;
           const package = await subscriptionsCollection.findOne({_id: ObjectID(id)});
           res.json(package);
+      })
+
+      //add subscription
+      app.post('/subscription', async(req,res)=>{
+          const data = req.body;
+          const result = await subscriptionsCollection.insertOne(data);
+          res.json(result);
+      })
+      //update subscription
+      app.put('/subscription/:id',async(req,res)=>{
+          const id = req.params.id;
+          const pack= req.body;
+          filter={_id:ObjectID(id)}
+          const options = { upsert: true };
+          const updateDoc={
+              $set:{
+                  package_name: pack.package_name,
+                  limit:  parseInt(pack.limit),
+                  price:  parseInt(pack.price)
+              }
+          }
+
+          const result = await subscriptionsCollection.updateOne(filter, updateDoc, options)
+          res.json(result);
+      })
+      //delete subscription
+      app.delete('/subscription/:id', async(req,res)=>{
+        const id = req.params.id;  
+        const result = await subscriptionsCollection.deleteOne({_id:ObjectID(id)})
+        res.json(result)
       })
 
       //user update package
@@ -172,7 +270,7 @@ async function run() {
             const preLimit = user.subscription.limit;
             const newLimit = sub.limit+preLimit;
             sub = {...sub, limit:newLimit}
-            console.log(sub);
+
         }
         const filter  = { email: email };
         const options = { upsert: true };
